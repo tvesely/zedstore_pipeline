a832c407715459d3b485fc51d7217ffdff2d297b
Alexandra Wang <lewang@pivotal.io>
2019-09-04 21:33:32 +0000

    Restart database before running benchmark queries
    Co-authored-by: Taylor Vesely <tvesely@pivotal.io>

--- concourse/scripts/zedstore_perf_test.sh
+++ concourse/scripts/zedstore_perf_test.sh
@@ -3,12 +3,18 @@
 set -xe
 
 : ${POSTGRES_INSTALL_DIR:=/usr/local/pgsql}
+: ${DATABASE_PATH:=/tmp/pg_db}
 
 if [ -d ${HOME}/workspace/zedstore_pipeline ]; then
     export ZEDSTORE_PIPELINE_SRC_PATH=${HOME}/workspace/zedstore_pipeline
 else
     export ZEDSTORE_PIPELINE_SRC_PATH=zedstore_pipeline
 fi
+
+${POSTGRES_INSTALL_DIR}/bin/psql -a -f ${ZEDSTORE_PIPELINE_SRC_PATH}/concourse/scripts/prepare_benchmark_ddl.sql |
+    perl -MTerm::ANSIColor -ane "if (/^(Time:|-- )/) { print color('bold blue'); print $_; print color('reset');} else { print $_ }"
+
+${POSTGRES_INSTALL_DIR}/bin/pg_ctl -D ${DATABASE_PATH} restart -l /tmp/pg_log
 
 ${POSTGRES_INSTALL_DIR}/bin/psql -a -f ${ZEDSTORE_PIPELINE_SRC_PATH}/concourse/scripts/benchmark.sql |
     perl -MTerm::ANSIColor -ane "if (/^(Time:|-- )/) { print color('bold blue'); print $_; print color('reset');} else { print $_ }"
